b4ef71665a94425e65bc906fd722e4326d4c1fba
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-02 20:43:26 -0300

    Implementação da Page Home

--- costs/src/components/pages/Home.jsx
+++ costs/src/components/pages/Home.jsx
@@ -1,8 +1,17 @@
+import styles from "./Home.module.css";
+import savings from "../../images/savings.svg";
+import LinkButton from "../layout/LinkButton";
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className={styles.home_container}>
+      <h1>
+        Bem vindo ao <span>Costs</span>
+      </h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="New Project" />
+      <img src={savings} alt="Costs" />
+    </section>
   );
 }
 
