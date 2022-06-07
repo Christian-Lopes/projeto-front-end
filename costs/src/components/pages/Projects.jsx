eea05cc8616cfced4f55653200e8b3802ba1dd68
Christian Lopes de Oliveira <christianpnn_@hotmail.com>
2022-06-07 14:40:46 -0300

    Criando lista de projetos

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -1,19 +1,29 @@
 import { useLocation } from "react-router-dom";
 
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+
+import styles from "./Projects.module.css";
 
 function Projects() {
   const location = useLocation();
-  let message = '';
+  let message = "";
 
   if (location.state) {
     message = location.state.message;
   }
 
   return (
-    <div>
-      <h1>Meus projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
       {message && <Message msg={message} type="success" />}
+      <Container customclass="start">
+        <p>Projetos</p>
+      </Container>
     </div>
   );
 }
